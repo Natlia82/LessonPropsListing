34905c2144dc94d3845d5bfd512d7fd58d7b8114
Natalia <nata@host>
2023-02-09 15:24:10 +0500

    получили json

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Listing from './components/Listing';
+import jsonData from './components/etsy.json';
 
 function App() {
-    return ( <
-        Listing / >
-    );
+    const loadData = JSON.parse(JSON.stringify(jsonData));
+   // console.log(loadData);
+ 
+    return ( <Listing items={loadData}/> );
 }
 
 export default App;
--- src/components/Listing.js
+++ src/components/Listing.js
@@ -1,19 +1,21 @@
 
 
-function Listing() {
-   return <div class="item-list">
-              <div class="item">
-                <div class="item-image">
-                    <a href="https://www.etsy.com/listing/292754135/woodland-fairy">
-                        <img src="https://img1.etsystatic.com/156/0/12814579/il_570xN.1173240751_50hv.jpg"/>
-                    </a>
-                </div>
-                <div class="item-details">
-                    <p class="item-title">Woodland Fairy</p>
-                    <p class="item-price">$3.99</p>
-                    <p class="item-quantity level-medium">12 left</p>
-                </div>
-               </div>
+function Listing(props) {
+    const { items } = props;
+    const itemList = items.map((item) => <div class="item">
+                                            <div class="item-image">
+                                                <a href="https://www.etsy.com/listing/292754135/woodland-fairy">
+                                                    <img src="https://img1.etsystatic.com/156/0/12814579/il_570xN.1173240751_50hv.jpg"/>
+                                                </a>
+                                            </div>
+                                            <div class="item-details">
+                                                <p class="item-title">Woodland Fairy</p>
+                                                <p class="item-price">$3.99</p>
+                                                <p class="item-quantity level-medium">12 left</p>
+                                            </div>
+                                        </div> )
+   return  <div class="item-list">
+              {itemList }
            </div>
 }
 export default Listing;
