5e1577ce81f405d39b58ac0b33974de62f52f280
Natalia <nata@host>
2023-02-19 20:20:40 +0500

    если нет картинки

--- src/components/Listing.js
+++ src/components/Listing.js
@@ -4,7 +4,9 @@ import Item from './item';
 function Listing(props) {
     const { items } = props;
   
-    const itemList = items.map((item) => <Item key={item.listing_id} item={item} />   )
+    const itemList = items.map((item) => { 
+                                        if (item.MainImage && item.MainImage.url_570xN) return  <Item key={item.listing_id} item={item} />  
+                                         })
    return  <div className="item-list">
               {itemList }
            </div>
